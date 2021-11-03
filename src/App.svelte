2d88b61ae90ba1ae5339bce0bea6c444db082768
Gyanendra Shukla <srirajshukla@gmail.com>
2021-11-03 10:51:45 +0530

    feat: inital commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
